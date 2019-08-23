ad2e255286b5ab7718aac954edb4cb3ab2344ab4
yuzhicheng <yuzhicheng@innobuddy.com>
2019-08-23 21:14:21 +0800

    add exception and error support

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,6 +1,10 @@
 /**
  * TODO:
- * []: return format, base on Zhike api style, with errors by middleware
+ * [*]: return format, base on Zhike api style, with errors by middleware ***
+ * []: 404 handler
+ * []: crsf
+ * []: etags
+ * []: compress
  * []: README.md
  * []: zignis make route a/b/c/d 'desc'
  * []: zignis serve -l list all routes
@@ -9,16 +13,19 @@
  * [*]: support router middleware
  * [*]: disable internal middleware
  * [*]: validation support
+ * []: change to winston logger
  * []: watch mode
  * []: view & template
+ * []: support sentry
  */
 const { Utils } = require('zignis')
 const path = require('path')
 const fs = require('fs')
 const requireDirectory = require('require-directory')
 const Koa = require('koa')
+const onerror = require('koa-onerror')
 const app = new Koa()
-
+onerror(app)
 const logger = require('koa-logger')
 const cors = require('kcors')
 const bodyParser = require('koa-bodyparser')
@@ -52,11 +59,10 @@ const travelRouter = (argv, routes, prefixPath = '') => {
                 cacheKey: ctx.url
               })
             } catch (e) {
-              ctx.body = e
-              return
+              throw new ctx.Exception(2, '', e)
             }
           }
-          ctx.body = await route.handler(ctx)
+          ctx.body.data = await route.handler(ctx)
         })
       }
 
@@ -74,7 +80,7 @@ exports.desc = 'simple server'
 
 exports.builder = function (yargs) {
   yargs.option('port', { default: false, describe: 'server port', alias: 'p' })
-  yargs.option('init-app', { default: false, describe: 'preprocess koa by application', alias: 'init' })
+  yargs.option('preprocess-koa', { default: false, describe: 'preprocess koa by application' })
   yargs.option('router-api-prefix', { default: '/api', describe: 'prefix all routes'})
   yargs.option('disable-internal-middleware-koa-logger', { describe: 'disable internal middleware koa-logger'})
   yargs.option('disable-internal-middleware-koa-bodyparser', { describe: 'disable internal middleware koa-bodyparser'})
@@ -92,12 +98,83 @@ exports.handler = async function (argv) {
   const publicDir = argv.publicDir ? path.resolve(argv.publicDir) : null
   const routes = argv.routeDir ? requireDirectory(module, path.resolve(appConfig.applicationDir, argv.routeDir)) : null
   travelRouter(argv, routes)
+  router['all']('*', ctx => {
+    throw new ctx.Exception(4)
+  })
 
   if (argv.initApp && fs.existsSync(path.resolve(appConfig.applicationDir, argv.initApp))) {
     require(path.resolve(appConfig.applicationDir, argv.initApp))(app)
   }
 
   argv.disableInternalMiddlewareKoaLogger || app.use(logger())
+
+  app.use(async (ctx, next) => {
+    ctx.errors = Object.assign({}, {
+      1: { msg: '未知错误', status: 500 },
+      2: { msg: '参数校验失败'},
+      4: { msg: '页面未找到', status: 400 }
+    }, ctx.errors)
+    class Exception extends Error {
+      constructor (code, msg, info) {
+        let errMsg
+        let status
+        let errCode = code || 1 // 默认 code = 1
+        if (msg || ctx.errors[errCode]) {
+          errMsg = msg || ctx.errors[errCode].msg || ''
+          status = ctx.errors[errCode].status || 200
+        } else {
+          errMsg = '未定义错误'
+          status = 500
+        }
+
+        super(errMsg)
+
+        this.code = errCode
+        this.msg = errMsg
+        this.status = status
+        this.info = info
+      }
+    }
+
+    ctx.Exception = Exception
+    try {
+      let startTime = new Date()
+      ctx.reqId = Utils.day(startTime).format('YYYYMMDD_HHmm_ssSSS') + '_' + Utils._.padStart(Utils._.random(0, 0xffffffff).toString(16), 8, 0)
+      ctx.body = {
+        reqId: ctx.reqId,
+        code: 0
+      }
+
+      await next()
+    } catch (e) {
+      console.error(e)
+      if (e instanceof Exception) { // 有准备的已知错误
+        ctx.status = e.status
+        ctx.body = {
+          reqId: ctx.reqId,
+          code: e.code,
+          msg: e.msg
+        }
+
+        if (e.info) {
+          ctx.body.info = e.info
+        }
+
+      } else { // 未知错误
+        ctx.status = 500
+        ctx.body = {
+          reqId: ctx.reqId,
+          code: 1,
+          msg: '未知错误'
+        }
+        
+      }
+      if (process.env.NODE_ENV !== 'production' && e) {
+        ctx.body.stack = e.stack // 调用栈返给前端
+      }
+    }
+  })
+
   argv.disableInternalMiddlewareKcors || app.use(cors({ credentials: true }))
   argv.disableInternalMiddlewareKoaBodyparser || app.use(bodyParser())
 
--- test/init.js
+++ test/init.js
@@ -1,6 +1,5 @@
 module.exports = (app) => {
   app.use(async (ctx, next) => {
-    console.log('here')
     await next()
   })
 
